277a107b066b25980f2fff25c4450263e8c0fa69
xinyuf2 <xinyuf2@outlook.com>
2020-04-17 16:40:42 +0800

    Initial commit

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Enemy States")]
+    [SerializeField]
+    float health = 100f;
+    [SerializeField]
+    int scoreValue = 150;
+
+    [Header("Shooting")]
+    [SerializeField]
+    float shotCounter;
+    [SerializeField]
+    float minTimeBetweenShots = 0.2f;
+    [SerializeField]
+    float maxTimeBetweenShots = 3f;
+    [SerializeField]
+    GameObject laserPrefab;
+    [SerializeField]
+    float projectileSpeed = 10f;
+
+    [Header("Sound Effects")]
+    [SerializeField]
+    GameObject deathVFX;
+    [SerializeField]
+    float durantionOfExplosion = 1f;
+    [SerializeField]
+    AudioClip deathSFX;
+    [SerializeField]
+    [Range(0, 1)]
+    float deathSFXVolumn = 0.75f;
+    [SerializeField]
+    AudioClip shootSFX;
+    [SerializeField]
+    [Range(0, 1)]
+    float shootSFXVolumn = 0.75f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+        AudioSource.PlayClipAtPoint(shootSFX, Camera.main.transform.position, shootSFXVolumn);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {  
+        DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
+        if (!damageDealer) { return; }
+        ProcessHit(damageDealer);
+    }
+
+    private void ProcessHit(DamageDealer damageDealer)
+    {
+        health -= damageDealer.GetDamage();
+        damageDealer.Hit();
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        FindObjectOfType<GameSession>().AddToScore(scoreValue);
+        Destroy(gameObject);
+        GameObject explosion = Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(explosion, durantionOfExplosion);
+        AudioSource.PlayClipAtPoint(deathSFX, Camera.main.transform.position, deathSFXVolumn);
+    }
+}
